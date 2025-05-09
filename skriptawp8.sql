933670889608666ec4572f155804fe5bddd06a0e
Dare80s <darioarambasic@outlook.com>
2025-05-09 18:07:10 +0200

    Kreiranje tablice smjer

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -1,1 +1,16 @@
-﻿create database edunovawp8;
+﻿--create database edunovawp8;
+
+
+--drop database edunovawp8;
+
+--use edunovawp8;
+
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+cijena decimal(18,2),
+datumpokretanja datetime,
+aktivan bit
+);
+
+
