c919294da972da7c1517aa217ec98a5ef51623c5
Dare80s <darioarambasic@outlook.com>
2025-05-09 18:38:24 +0200

    Kreiranje tablice smjerovi

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -21,11 +21,15 @@
 --);
 --drop table polaznici;
 
-create table polaznici(
-sifra int,
-ime varchar(50),
-prezime varchar(50),
-email varchar(100)
-);
+--create table polaznici(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--email varchar(100)
+--);
 
+--create table clanovi(
+--grupa int,
+--polaznik int
+--);
 
