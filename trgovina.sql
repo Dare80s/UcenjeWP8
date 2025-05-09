ee49efd3214521c2afb3bf0f45e8864c92230658
Dare80s <darioarambasic@outlook.com>
2025-05-09 19:31:23 +0200

    Ispravak tablice stavke

--- trgovina.sql
+++ trgovina.sql
@@ -14,8 +14,10 @@
 --cijena decimal(18,2)
 --);
 
---create table stavke(
---racun int,
---proizvod int,
---količna decimal(5,3)
---);
+create table stavke(
+racun int,
+proizvod int,
+kolicina decimal(5,3)
+);
+
+
