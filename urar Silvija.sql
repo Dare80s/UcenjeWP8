a766fc9438b56da2e32cb48c7f309831bb3f685e
Dare80s <darioarambasic@outlook.com>
2025-05-11 15:13:29 +0200

    Kreiranje tablice šegrti

--- urar Silvija.sql
+++ urar Silvija.sql
@@ -9,10 +9,16 @@ prezime varchar(50),
 kontakt varchar(100)
 ;)
 
---create table sat(
---sat id int,
---marka varchar (50),
---model varchar(50),
---serijski_broj varchar(100)
---korisnikid int
+create table sat(
+sat id int,
+marka varchar (50),
+model varchar(50),
+serijski_broj varchar(100)
+korisnikid int
+;)
+
+--create table segrti(
+--segrtid int,
+--ime varchar(50),
+--prezime varchar(50)
 --;)
