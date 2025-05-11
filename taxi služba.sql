f42abd2f8a43481480008beb64bcca2be882f605
Dare80s <darioarambasic@outlook.com>
2025-05-11 10:52:39 +0200

    Kreiranje tablice vozilo

--- taxi služba.sql
+++ taxi služba.sql
@@ -1,1 +1,10 @@
---create database taxi sluzba;
+create database taxi sluzba;
+
+
+--create table vozilo(
+--id int,
+--registracija varchar(20),
+--marka varchar(50),
+--model varchar(50),
+--godina_proizvodnje int
+--;)
