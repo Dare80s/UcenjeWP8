0c0e87d45ade3ba14fd71a3177cd4be270f3775c
Dare80s <darioarambasic@outlook.com>
2025-05-11 15:08:42 +0200

    Kreiranje tablice sat

--- urar Silvija.sql
+++ urar Silvija.sql
@@ -2,9 +2,17 @@
 
 
 
---create table korisnik(
---korisnikid int,
---ime varchar(50),
---prezime varchar(50),
---kontakt varchar(100)
+create table korisnik(
+korisnikid int,
+ime varchar(50),
+prezime varchar(50),
+kontakt varchar(100)
+;)
+
+--create table sat(
+--sat id int,
+--marka varchar (50),
+--model varchar(50),
+--serijski_broj varchar(100)
+--korisnikid int
 --;)
