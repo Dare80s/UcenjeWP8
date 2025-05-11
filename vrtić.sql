8abf34f952459b79286b582cf5770e4b95082de9
Dare80s <darioarambasic@outlook.com>
2025-05-11 09:56:27 +0200

    Ispravak tablice odgajateljica

--- vrtić.sql
+++ vrtić.sql
@@ -1,15 +1,16 @@
-﻿create database vrtic;
-
-create table dijete;
+﻿
 
 create table odgajateljica(
 id int,
 ime varchar(50),
 prezime varchar(50),
-datum rodenja date,
+datum_rodenja date,
 email varchar(100),
 telefon varchar(20)
 );
+
+
+
 
 
 
