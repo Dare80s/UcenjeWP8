8bb0a7b557672a27519aa263832b746a2c2fea6f
Dare80s <darioarambasic@outlook.com>
2025-05-11 15:04:08 +0200

    Kreiranje tablice korisnik

--- urar Silvija.sql
+++ urar Silvija.sql
@@ -1,1 +1,10 @@
-﻿--create database urar silvija;
+﻿create database urar silvija;
+
+
+
+--create table korisnik(
+--korisnikid int,
+--ime varchar(50),
+--prezime varchar(50),
+--kontakt varchar(100)
+--;)
