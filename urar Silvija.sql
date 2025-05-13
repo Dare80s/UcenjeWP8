577bc00caf81cd604582743eafad605c11979bf0
Dare80s <darioarambasic@outlook.com>
2025-05-13 19:56:14 +0200

    završetak u školi nastaviti kod kuće

--- urar Silvija.sql
+++ urar Silvija.sql
@@ -1,5 +1,11 @@
-﻿create database urar silvija;
-
+﻿--create database urar silvija;
+use master;
+go
+drop database if exists urarsilvija;
+go
+create database urarsilvija;
+go
+use urarsilvija;
 
 
 create table korisnik(
@@ -7,25 +13,25 @@ korisnikid int,
 ime varchar(50),
 prezime varchar(50),
 kontakt varchar(100)
-;)
+);
 
 create table sat(
-sat id int,
+sat_id int,
 marka varchar (50),
 model varchar(50),
 serijski_broj varchar(100)
-korisnikid int
-;)
+korisnik_id int
+);
 
 create table segrti(
 segrtid int,
 ime varchar(50),
 prezime varchar(50)
-;)
+);
 
---create table popravak(
---satid int,
---datum_popravka date,
---opis_popravka varchar(255),
---segrtid int
---;)
+create table popravak(
+satid int,
+datum_popravka date,
+opis_popravka varchar(255),
+segrtid int
+);
