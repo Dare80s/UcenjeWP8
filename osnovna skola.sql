553c426a06c4da6bc85f1dae64f8171909addb25
Dare80s <darioarambasic@outlook.com>
2025-05-16 11:30:49 +0200

    Kreiranje tablice dijete

--- osnovna skola.sql
+++ osnovna skola.sql
@@ -1,1 +1,8 @@
-﻿--create database osnovna skola;
+﻿create database osnovna skola;
+
+--create table dijete(
+--dijete int primary key,
+--ime varchar(50) not null,
+--prezime varchar(50)not null,
+--datum rodenja date
+--);
