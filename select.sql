4f27397375cf8d40196bedd95c570a71c21dc538
Dare80s <darioarambasic@outlook.com>
2025-05-16 18:02:45 +0200

    rad na unosu podataka

---
+++ select.sql
@@ -0,0 +1,24 @@
+﻿select * from smjerovi;
+
+--1 ->
+use edunovawp8;
+
+insert into smjerovi (naziv,cijena,datumpokretanja,aktivan)
+values ('Web programiranje',1200.54,'2025-05-16 17:00:01',1);
+
+insert into smjerovi (naziv) values
+--2
+('Serviser'),
+--3
+('Web dizajn'),
+--4
+('Marketing');
+
+insert into grupe(naziv,smjer)
+values
+--1
+('WP8',1),
+--2
+('WP7',1),
+--3
+('S1',2);
