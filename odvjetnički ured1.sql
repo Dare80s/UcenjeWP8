3c04f108abfe5c2d6d32d8399d5fd1ab56be63f3
Dare80s <darioarambasic@outlook.com>
2025-05-17 17:09:34 +0200

    Kreiranje tablice klijent

--- odvjetnički ured1.sql
+++ odvjetnički ured1.sql
@@ -1,14 +1,14 @@
 ﻿create database odvjetnicki ured;
 
---CREATE TABLE odvjetnik; 
---id SERIAL PRIMARY KEY,
---ime TEXT NOT NULL
---);
-
-CREATE TABLE klijent (
+CREATE TABLE odvjetnik; 
 id SERIAL PRIMARY KEY,
 ime TEXT NOT NULL
 );
+
+--CREATE TABLE klijent (
+--id SERIAL PRIMARY KEY,
+--ime TEXT NOT NULL
+--);
 
 CREATE TABLE suradnik (
  id SERIAL PRIMARY KEY,
