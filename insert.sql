d2e2bd61da867f625bb4446e1a584a26d06eb254
Dare80s <darioarambasic@outlook.com>
2025-05-17 15:07:56 +0200

    unos podatak insert

--- insert.sql
+++ insert.sql
@@ -1,30 +1,36 @@
-﻿
+﻿use edunovawp8;
 
---1 ->
-use edunovawp8;
+select * from smjerovi;
 
+-- 1   -> ovo je šifra koju će dodijeliti baza
 insert into smjerovi (naziv,cijena,datumpokretanja,aktivan)
 values ('Web programiranje',1200.54,'2025-05-16 17:00:01',1);
 
 insert into smjerovi (naziv) values
---2
+-- 2
 ('Serviser'),
---3
+-- 3
 ('Web dizajn'),
---4
+-- 4
 ('Marketing');
 
-insert into grupe(naziv,smjer)
-values
---1
-('WP8',1),
---2
+-- šifre mogu biti i ovog oblika (GUID):
+-- 3126f6a3-f2f7-485e-34c5-08dd924f8055
+-- 9f383e18-fc7a-4059-a641-e73f4d661fc3
+
+select * from grupe;
+
+insert into grupe (naziv,smjer) 
+values 
+-- 1
+('WP8',1), -- 'WP8',1 (broj 1 je vanjski ključ koji odgovara šifri smjera Web programiranje)
+-- 2
 ('WP7',1),
---3
+-- 3
 ('S1',2);
 
 select * from polaznici;
---  1 - 29
+-- 1 - 29
 insert into polaznici (prezime, ime, email) values
 ('Gavran','Barbara','barbaragavran67@gmail.com'),
 ('Šteko','Ivan','ivan.steko5@gmail.com'),
@@ -59,8 +65,11 @@ insert into polaznici (prezime, ime, email) values
 select * from clanovi;
 
 insert into clanovi (grupa,polaznik) values
-(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10),(1,11),(1,12),(1,13),(1,14),(1,15),(1,16),
-(1,17),(1,18),(1,19),(1,20),(1,21),(1,23),(1,24),(1,25),(1,26),(1,27),(1,28),(1,29);
+(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10),
+(1,11),(1,12),(1,13),(1,14),(1,15),(1,16),(1,17),(1,18),(1,19),(1,20),
+(1,21),(1,22),(1,23),(1,24),(1,25),(1,26),(1,27),(1,28),(1,29);
 
 insert into clanovi(grupa,polaznik) values
 (3,7),(3,17),(3,27);
+
+
