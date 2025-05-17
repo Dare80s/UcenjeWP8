e84e329e371726ced4df7e1dd4d7e78e1e91a149
Dare80s <darioarambasic@outlook.com>
2025-05-17 17:10:46 +0200

    Kreiranje datoteke odvjetnički ured

--- odvjetnički ured1.sql
+++ odvjetnički ured1.sql
@@ -1,14 +1,14 @@
-﻿create database odvjetnicki ured;
+﻿--create database odvjetnicki ured;
 
 CREATE TABLE odvjetnik; 
 id SERIAL PRIMARY KEY,
 ime TEXT NOT NULL
 );
 
---CREATE TABLE klijent (
---id SERIAL PRIMARY KEY,
---ime TEXT NOT NULL
---);
+CREATE TABLE klijent (
+id SERIAL PRIMARY KEY,
+ime TEXT NOT NULL
+);
 
 CREATE TABLE suradnik (
  id SERIAL PRIMARY KEY,
