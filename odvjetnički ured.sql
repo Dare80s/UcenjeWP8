7936705164607cebd4fee5f13d97ab1a118771e6
Dare80s <darioarambasic@outlook.com>
2025-05-17 15:59:32 +0200

    Kreiranje tablice obrana suradnik

--- odvjetnički ured.sql
+++ odvjetnički ured.sql
@@ -15,9 +15,15 @@ CREATE TABLE suradnik (
  ime TEXT NOT NULL
 );
 
---CREATE TABLE obrana (
---id SERIAL PRIMARY KEY,
---odvjetnik_id INT REFERENCES odvjetnik(id),
---klijent_id INT REFERENCES klijent(id),
---datum DATE NOT NULL
+CREATE TABLE obrana (
+id SERIAL PRIMARY KEY,
+odvjetnik_id INT REFERENCES odvjetnik(id),
+klijent_id INT REFERENCES klijent(id),
+datum DATE NOT NULL
+);
+
+--CREATE TABLE obrana_suradnik (
+--obrana_id INTEGER REFERENCES obrana(id),
+--suradnik_id INTEGER REFERENCES suradnik(id),
+--PRIMARY KEY (obrana_id, suradnik_id)
 --);
