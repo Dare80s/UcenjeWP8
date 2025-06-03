e0df15fed8235c68f6d622a55e0789818b510741
Dare80s <darioarambasic@outlook.com>
2025-06-03 19:52:53 +0200

    UlazIzlaz

---
+++ BACKEND/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+public static void Izvedi()
+        {
+
+
+
+            Console.Write("Pozdrav svijetu");
+
+            Console.WriteLine("Isti red");
+
+            Console.WriteLine("Osijek je\nSUPER");
+
+            Console.WriteLine("\t\tOvo je uvučeno");
+
+            Console.Write("Unesi svoje ime:");
+
+            // varijabla ime
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste {0}", ime);
+
+        } // kraj metode
+
+
+    } // kraj klase
+} // kraj imenskog prostora (namespace)
