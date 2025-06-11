6b150984ce78dee2e97089fb525f019dd15795ab
Dare80s <darioarambasic@outlook.com>
2025-06-11 16:29:54 +0200

    UlazIzlaz

---
+++ BACKEND/Ucenje/E07ForPetlja.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E07ForPetlja
+    {
+
+
+        public static void Izvedi()
+        {
+
+
+            Console.WriteLine("E07 for");
+
+
+            Console.WriteLine("Hrvatska - Češka 5:1");
+            Console.WriteLine("Hrvatska - Češka 5:1");
+            Console.WriteLine("Hrvatska - Češka 5:1");
+            Console.WriteLine("Hrvatska - Češka 5:1");
+            Console.WriteLine("Hrvatska - Češka 5:1");
+            Console.WriteLine("Hrvatska - Češka 5:1");
+            Console.WriteLine("Hrvatska - Češka 5:1");
+            Console.WriteLine("Hrvatska - Češka 5:1");
+            Console.WriteLine("Hrvatska - Češka 5:1");
+            Console.WriteLine("Hrvatska - Češka 5:1");
+
+
+
+            Console.WriteLine("***************************");
+
+            for(int i=0; i<1000000000; i++)  //ne dolazi ; // ispiši milijun puta
+            {
+                Console.WriteLine("Hrvatska - Češka 5:1");
+            }
+
+            Console.WriteLine("*************");
+            //ispiši prvih 100000 brojeva
+
+            for(int i = 0; i<1000000; i++)
+            
+                Console.WriteLine("{0}." i+1); 
+
+
+            
+            
+
+
+            Console.WriteLine("**********");
+            //ispiši zbroj prvih 100 brojeva
+
+            Console.WriteLine((100/2)*100 + 100/2 ); //4 operacije
+            Console.WriteLine(100/2 * (100 +1);
+           
+
+        
+    }
+}
--- BACKEND/Ucenje/Program.cs
+++ BACKEND/Ucenje/Program.cs
@@ -7,4 +7,4 @@ E01UlazIzlaz.Izvedi();
 
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
